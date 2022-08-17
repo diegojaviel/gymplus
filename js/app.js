8501abb058b3dfc6949e8b5f34c84a4e6764acfe
Diego Javiel <diegojaviel@gmail.com>
2022-08-17 16:07:32 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// SHOW MENU
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+/* Validacao se constante existe*/
+
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
+
+
+// MENU HIDDEN
+/* Validacao se constante existe*/
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
+
+
+// REMOVE MENU MOBILE
+
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n=> n.addEventListener('click', linkAction))
