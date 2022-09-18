c1f0acc30b6c718c5f1b4bd12f26081597728e54
Luis Pellizzon <luis.pellizzon@gmail.com>
2022-09-18 21:21:36 +0100

    Changing Title pages

--- js/observer.js
+++ js/observer.js
@@ -1,6 +1,7 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
+      console.log(entry);
       entry.target.classList.add("show");
     } else {
       entry.target.classList.remove("show");
